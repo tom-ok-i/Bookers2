6d8b83f85b2da9fc724088828e5653acf52b706b
tom-ok-i <ten21.t.tom@gmail.com>
2021-07-22 12:47:13 +0000

    [update]bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,7 +25,7 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(@book.id)
+    redirect_to book_path(book.id)
   end 
   
   
