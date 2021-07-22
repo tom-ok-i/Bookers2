a9ac7e4208436e3d7ffb64b245704df7da89ee12
tom-ok-i <ten21.t.tom@gmail.com>
2021-07-22 09:59:29 +0000

    [update]bookers2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'homes#top'
   devise_for :users
+  root to: 'homes#top'
+  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
+  resources :users, only: [:show, :edit, :update, :index]
+  get 'about', to: "homes#about"
 end
